766c524d58631e4659c64b685227e0889894b0ce
Julien Dephix <jdephix@hotmail.com>
2019-02-13 08:17:26 +0100

    add support for book upload via a CSV file

--- assets/vue/App.vue
+++ assets/vue/App.vue
@@ -2,6 +2,7 @@
     <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <router-link class="navbar-brand" to="/home"><i class="fas fa-home"></i></router-link>
+            <router-link class="navbar-brand" to="/books"><i class="fas fa-book"></i></router-link>
         </nav>
 
         <router-view></router-view>

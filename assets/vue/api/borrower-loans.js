07f32cf48747229b7fc464a9f61cea1463148ed0
Julien Dephix <jdephix@hotmail.com>
2019-02-15 06:29:54 +0100

    users can borrow books

--- assets/vue/api/borrower-loans.js
+++ assets/vue/api/borrower-loans.js
@@ -3,5 +3,8 @@ import axios from 'axios'
 export default {
     getLoans(id) {
         return axios.get(`/api/loans/by-user/${id}`)
+    },
+    saveLoan(loanInfo) {
+        return axios.post(`/api/loans/by-user/${loanInfo.borrower.id}/${loanInfo.code}`)
     }
 }

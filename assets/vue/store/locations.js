53a64a03fc25932b4b7cc530d27b8eb3b807a61e
Jools <jdephix@hotmail.com>
2019-09-20 07:07:39 +0200

    add book count in location page (#36)
    * add book count in location page

--- assets/vue/store/locations.js
+++ assets/vue/store/locations.js
@@ -21,20 +21,6 @@ export default {
         setLocations(state, locations) {
             state.locations = locations
         },
-        setCurrent(state, location) {
-            state.currentLocation = location
-        },
-        addLocation(state, location) {
-            let locationIndex = state.locations.findIndex( u => u.id === location.id)
-            if (locationIndex !== -1) {
-                state.locations.splice(locationIndex, 1, location)
-            } else {
-                state.locations.push(location)
-            }
-        },
-        deleteLocation(state, locationId) {
-            state.locations = state.locations.filter( u => u.id !== locationId)
-        }
     },
 
     actions: {
@@ -42,16 +28,6 @@ export default {
             return LocationsAPI
                 .getAll()
                 .then(res => commit('setLocations', res.data))
-        },
-        save({commit}, payload) {
-            return LocationsAPI
-                .save(payload)
-                .then(res => commit('addLocation', res.data))
-        },
-        delete({commit}, locationId) {
-            return LocationsAPI
-                .delete(locationId)
-                .then( res => commit('deleteLocation', locationId))
         },
     }
 }
